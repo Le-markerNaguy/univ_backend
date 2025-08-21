d1d737a4099e1d4b301b2fd37ee78cc5a4b48d25
Le-markerNaguy <naguyjuccedem@email.com>
2025-08-22 01:17:07 +0200

    Correction URL frontend en ligne

--- src/routes/superadmin.route.ts
+++ src/routes/superadmin.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authorizeRoles , authenticate } from '../middlewares/auth.middleware';
-import { creerUniversite, creerAdmin, postSuperAdmin , afficherUniversites , afficherAdmins} from '../controllers/superAdmin.controller';
+import { creerUniversite, creerAdmin, postSuperAdmin , afficherUniversites , afficherAdmins } from '../controllers/superAdmin.controller';
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.post('/', postSuperAdmin);
 
 // Toutes les routes suivantes sont protégées par le rôle SUPER_ADMIN
 router.post('/universites', authenticate , authorizeRoles('SUPER_ADMIN'), creerUniversite);
+router.post('/', authenticate , authorizeRoles('SUPER_ADMIN'), postSuperAdmin);
 router.post('/admins', authenticate , authorizeRoles('SUPER_ADMIN'), creerAdmin);
 router.get('/universites', authenticate , authorizeRoles('SUPER_ADMIN'), afficherUniversites);
 router.get('/admins', authenticate , authorizeRoles('SUPER_ADMIN'), afficherAdmins);
